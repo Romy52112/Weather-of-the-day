8e554bc65f250243c36fb8318fe8a7b736996ce0
Romy Pisigan <romymortilpisigan@gmail.com>
2024-03-18 02:15:36 -0700

    getting the temp,wind and humidity

--- assets/weather.js
+++ assets/weather.js
@@ -1,7 +1,13 @@
 const apiKey = '5a51afcc1b30877aabfe03da14554523';
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
 
-const outputElement = document.getElementById('resultPage');
+var searchBtn = document.getElementById('searchBtn')
+// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?${cityName}&appid=${apiKey}`;
+
+searchBtn.addEventListener('click', function(){
+var cityName = document.getElementById('userInput').value
+var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+
+var outputElement = document.getElementById('resultPage'); 
 
 fetch(apiUrl)
   .then(response => {
@@ -11,13 +17,62 @@ fetch(apiUrl)
     return response.json();
   })
   .then(data => {
+    console.log(data)
     const temperature = data.main.temp;
     const description = data.weather[0].description;
-    const location = data.name;
-    outputElement.innerHTML = `<p>Temperature in ${location}: ${temperature}°C</p>
-                               <p>Weather: ${description}</p>`;
+    const location = data.name; 
+    const wind = data.wind.speed;
+    const humidity = data.main.humidity; 
+    
+
+
+    outputElement.innerHTML = `<h3> ${location} </h3>
+                               <p>Temperature: ${temperature}°F</p>
+                               <p>Weather: ${description}</p> 
+                               <p>Wind: ${wind} MPH</p>
+                               <p>Humidity: ${humidity} %</p>`;
+                               
+
   })
   .catch(error => {
     console.error('Error:', error);
   });
+  console.log()
+} )
+
+
+
+
+searchBtn.addEventListener('click', function(){
+  var cityName = document.getElementById('userInput').value
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast/?q=' + cityName + '&appid=' + apiKey + '&units=imperial' + '&cnt=5';
   
+  var forecastEl = document.getElementById('forecast'); 
+  
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+   
+      
+
+      // const description = data.weather[0].description;
+      // const wind = data.wind.speed;
+      // const humidity = data.main.humidity; 
+  
+  
+      //  forecastEl.innerHTML 
+   
+                                 
+  
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    console.log()
+  } )
